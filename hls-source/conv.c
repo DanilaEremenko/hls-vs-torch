a5318fcde235607a327bfe531396ef735b8df520
Danila <danilaeremenko@mail.ru>
2021-12-20 15:29:02 +0300

    hls sources refactored

--- hls-source/conv.c
+++ hls-source/conv.c
@@ -7,11 +7,11 @@ void conv(
 {
 
 LOOP_GLOBAL_HEIGHT:
-  for (int g_row = 0; g_row < EXPANDED_HEIGHT - CONV_SIZE + 1; g_row++)
+  for (int g_row = 0; g_row < TARGET_HEIGHT; g_row++)
   {
 
   LOOP_GLOBAL_WIDTH:
-    for (int g_col = 0; g_col < EXPANDED_WIDTH - CONV_SIZE + 1; g_col++)
+    for (int g_col = 0; g_col < TARGET_WIDTH; g_col++)
     {
       int sum = 0;
 
@@ -21,7 +21,6 @@ LOOP_GLOBAL_HEIGHT:
       LOOP_LOCAL_WIDTH:
         for (int l_col = 0; l_col < CONV_SIZE; l_col++)
         {
-          // printf("sum[%d][%d][%d][%d] = %f * %f\n", g_row, g_col, l_row, l_col, input_img[l_row][l_col], conv_matrix[l_row][l_col]);
           sum += input_img[g_row + l_row][g_col + l_col] * conv_matrix[l_row][l_col];
         }
       }
--- hls-source/conv.h
+++ hls-source/conv.h
@@ -3,9 +3,9 @@
 #include <stdlib.h>
 #define EXPANDED_HEIGHT 3332
 #define EXPANDED_WIDTH 5721
-#define TARGET_HEIGHT EXPANDED_HEIGHT - 2
-#define TARGET_WIDTH EXPANDED_WIDTH - 2
 #define CONV_SIZE 3
+#define TARGET_HEIGHT EXPANDED_HEIGHT - (CONV_SIZE - 1)
+#define TARGET_WIDTH EXPANDED_WIDTH - (CONV_SIZE - 1)
 
 typedef float data_sc;
 void conv(
