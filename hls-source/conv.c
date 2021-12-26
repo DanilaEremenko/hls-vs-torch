f4bbfed72ab58a5a2ea9ec780272a679a1b728e2
Danila <danilaeremenko@mail.ru>
2021-12-26 19:07:33 +0300

    loop renamed in C-source, perfect loop refactored

--- hls-source/conv.c
+++ hls-source/conv.c
@@ -6,19 +6,19 @@ void conv(
     data_sc conv_matrix[CONV_SIZE][CONV_SIZE])          // матрица свертки
 {
 
-LOOP_GLOBAL_HEIGHT: // цикл по строчкам исходного изображения
+L1: // цикл по строчкам исходного изображения
   for (int g_row = 0; g_row < TARGET_HEIGHT; g_row++)
   {
 
-  LOOP_GLOBAL_WIDTH: // цикл по столбцам исходного изображения
+  L2: // цикл по столбцам исходного изображения
     for (int g_col = 0; g_col < TARGET_WIDTH; g_col++)
     {
       int sum = 0;
 
-    LOOP_LOCAL_HEIGHT: // цикл по строчкам матрицы свертки
+    L3: // цикл по строчкам матрицы свертки
       for (int l_row = 0; l_row < CONV_SIZE; l_row++)
       {
-      LOOP_LOCAL_WIDTH: // цикл по столбцам матрицы свертки
+      L4: // цикл по столбцам матрицы свертки
         for (int l_col = 0; l_col < CONV_SIZE; l_col++)
         {
           sum += input_img[g_row + l_row][g_col + l_col] * conv_matrix[l_row][l_col]; //умножение текущего окна исходного изображения на матрицу свертки
--- hls-source/conv_beatifull.c
+++ hls-source/conv_beatifull.c
@@ -5,27 +5,13 @@ void conv(
     data_sc output_img[TARGET_HEIGHT][TARGET_WIDTH],    // выходная матрица
     data_sc conv_matrix[CONV_SIZE][CONV_SIZE])          // матрица свертки
 {
-  int sum = 0;
-LOOP_GLOBAL_HEIGHT: // цикл по строчкам исходного изображения
+L1: // цикл по строчкам исходного изображения
   for (int g_row = 0; g_row < TARGET_HEIGHT; g_row++)
-  {
-
-  LOOP_GLOBAL_WIDTH: // цикл по столбцам исходного изображения
+  L2: // цикл по столбцам исходного изображения
     for (int g_col = 0; g_col < TARGET_WIDTH; g_col++)
-    {
-    LOOP_LOCAL_HEIGHT: // цикл по строчкам матрицы свертки
+    L3: // цикл по строчкам матрицы свертки
       for (int l_row = 0; l_row < CONV_SIZE; l_row++)
-      {
-      LOOP_LOCAL_WIDTH: // цикл по столбцам матрицы свертки
+      L4: // цикл по столбцам матрицы свертки
         for (int l_col = 0; l_col < CONV_SIZE; l_col++)
-        {
-          if (l_row == 0 && l_col == 0) // начало свертки для текущего окна изображения
-            sum = 0;
-          sum += input_img[g_row + l_row][g_col + l_col] * conv_matrix[l_row][l_col]; //умножение текущего окна исходного изображения на матрицу свертки
-          if (l_row == (CONV_SIZE - 1) && l_col == (CONV_SIZE - 1)) // сохранение результата свертки для текущего окна изображения
-            output_img[g_row][g_col] = sum;
-        }
-      }
-    }
-  }
+          output_img[g_row][g_col] += input_img[g_row + l_row][g_col + l_col] * conv_matrix[l_row][l_col]; //умножение текущего окна исходного изображения на матрицу свертки
 }
